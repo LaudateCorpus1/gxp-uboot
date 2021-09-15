ea5402ae7e38f4ddb1fe71aa0c6e8da9ee6fefc2
Jorge Cisneros <jorge.cisneros@hpe.com>
2021-09-15 14:57:28 -0500

    Update GXP I2C drive to the new model driver, using DM
    Signed-off-by: Jorge Cisneros <jorge.cisneros@hpe.com>

--- drivers/i2c/gxp_i2c.c
+++ drivers/i2c/gxp_i2c.c
@@ -3,220 +3,214 @@
  * GXP I2C driver
  *
  * (C) Copyright 2021 Hewlett Packard Enterprise Development LP.
- * Author: Gilbert Chen <gilbert.chen@hpe.com>
+ * Author: Jorge Cisneros <jorge.cisneros@hpe.com>
  */
 
-
 #include <common.h>
+#include <dm.h>
 #include <asm/io.h>
 #include <linux/delay.h>
-
-#define GXP_IOP_BASE	0xC0000000
-
-#define GXP_I2C0_BASE		(GXP_IOP_BASE + 0x2000)
-#define GXP_I2C1_BASE		(GXP_IOP_BASE + 0x2100)
-#define GXP_I2C2_BASE		(GXP_IOP_BASE + 0x2200)
-#define GXP_I2C3_BASE		(GXP_IOP_BASE + 0x2300)
-#define GXP_I2C4_BASE		(GXP_IOP_BASE + 0x2400)
-#define GXP_I2C5_BASE		(GXP_IOP_BASE + 0x2500)
-#define GXP_I2C6_BASE		(GXP_IOP_BASE + 0x2600)
-#define GXP_I2C7_BASE		(GXP_IOP_BASE + 0x2700)
-#define GXP_I2C8_BASE		(GXP_IOP_BASE + 0x2800)
-#define GXP_I2C9_BASE		(GXP_IOP_BASE + 0x2900)
+#include <i2c.h>
+#include <log.h>
+#include <clk.h>
 
 /* GXP I2C registers */
-#define GXP_I2CSTAT	0x00
-#define GXP_I2CEVTERR	0x01
-#define GXP_I2CSNPDAT	0x02
-#define GXP_I2CMCMD	0x04
-#define GXP_I2CMTXDAT	0x05
-#define GXP_I2CSCMD	0x06
-#define GXP_I2CSTXDAT	0x07
-#define GXP_I2CMANCTRL	0x08
-#define GXP_I2CSNPAA	0x09
-#define GXP_I2COWNADR	0x0B
-#define GXP_I2CFREQDIV	0x0C
-#define GXP_I2CFLTFAIR	0x0D
-#define GXP_I2CTMOEDG	0x0E
-#define GXP_I2CCYCTIM	0x0F
+#define GXP_I2CSTAT 0x00
+#define GXP_I2CEVTERR 0x01
+#define GXP_I2CSNPDAT 0x02
+#define GXP_I2CMCMD 0x04
+#define GXP_I2CMTXDAT 0x05
+#define GXP_I2CSCMD 0x06
+#define GXP_I2CSTXDAT 0x07
+#define GXP_I2CMANCTRL 0x08
+#define GXP_I2CSNPAA 0x09
+#define GXP_I2COWNADR 0x0B
+#define GXP_I2CFREQDIV 0x0C
+#define GXP_I2CFLTFAIR 0x0D
+#define GXP_I2CTMOEDG 0x0E
+#define GXP_I2CCYCTIM 0x0F
 
-#define GXP_I2C_BUS_NUM	9
-#define POLLTIME_US	100000
-enum {
+#define POLLTIME_US 100000
+enum
+{
 	GXP_I2C_IDLE = 0,
 	GXP_I2C_STARTED,
 };
 
-static u32 i2c_base[GXP_I2C_BUS_NUM] = {
-	GXP_I2C0_BASE, GXP_I2C1_BASE, GXP_I2C2_BASE,
-	GXP_I2C3_BASE, GXP_I2C4_BASE, GXP_I2C5_BASE,
-	GXP_I2C6_BASE, GXP_I2C7_BASE, GXP_I2C8_BASE};
-
-static unsigned int bus_initialized[GXP_I2C_BUS_NUM];
-static unsigned int bus_state[GXP_I2C_BUS_NUM];
-static unsigned int current_bus;
-
-static void i2c_bus_init(unsigned int bus)
-{
-	u32 base = i2c_base[bus];
-
-	bus_state[bus] = GXP_I2C_IDLE;
-	bus_initialized[bus] = 1;
-
-	writeb(0x14, base + GXP_I2CFREQDIV);	//clock = 100KHz
-	writeb(0x61, base + GXP_I2CFLTFAIR);	//filter count = 6, fairness count = 1
-	writeb(0x0a, base + GXP_I2CTMOEDG);
-	writeb(0x00, base + GXP_I2CCYCTIM);	//disable maximum cycle timeout
-
-	writeb(0x80, base + GXP_I2CMCMD);	//clear event
-	writeb(0x30, base + GXP_I2CSCMD);	//mask slave event
-	writeb(0x00, base + GXP_I2CEVTERR);	//clear event
-
-	writel(0x000000f0, base + GXP_I2CMANCTRL); // reset the engine
-	udelay(10);
-	writel(0x00000030, base + GXP_I2CMANCTRL);
-}
-
-static void gxp_i2c_stop(void)
+struct gxp_i2c_priv
 {
 	u32 base;
+	uint8_t state;
+};
 
-	base = i2c_base[current_bus];
-	writeb(0x82, base + GXP_I2CMCMD); // clear event, send stop
-	bus_state[current_bus] = GXP_I2C_IDLE;
+static void gxp_i2c_init_bus(struct udevice *dev)
+{
+	struct gxp_i2c_priv *priv = dev_get_priv(dev);
+
+	priv->base = (u32)dev_read_addr_ptr(dev);
+	priv->state = GXP_I2C_IDLE;
+
+	debug("Base  %x \n", priv->base);
+
+	writeb(0x14, priv->base + GXP_I2CFREQDIV); //clock = 100KHz
+	writeb(0x61, priv->base + GXP_I2CFLTFAIR); //filter count = 6, fairness count = 1
+	writeb(0x0a, priv->base + GXP_I2CTMOEDG);
+	writeb(0x00, priv->base + GXP_I2CCYCTIM); //disable maximum cycle timeout
+
+	writeb(0x80, priv->base + GXP_I2CMCMD);	  //clear event
+	writeb(0x30, priv->base + GXP_I2CSCMD);	  //mask slave event
+	writeb(0x00, priv->base + GXP_I2CEVTERR); //clear event
+
+	writel(0x000000f0, priv->base + GXP_I2CMANCTRL); // reset the engine
+	udelay(10);
+	writel(0x00000030, priv->base + GXP_I2CMANCTRL);
 }
 
-static int gxp_wait_event(void)
+static void gxp_i2c_stop(struct udevice *dev)
+{
+	struct gxp_i2c_priv *priv = dev_get_priv(dev);
+
+	writeb(0x82, priv->base + GXP_I2CMCMD); // clear event, send stop
+	priv->state = GXP_I2C_IDLE;
+}
+
+static int gxp_wait_event(struct udevice *dev)
 {
 	unsigned int timeout;
 	unsigned int value;
-	u32 base;
+	struct gxp_i2c_priv *priv = dev_get_priv(dev);
 
-	base = i2c_base[current_bus];
 	/* wait for master event*/
 	timeout = 0;
-	value = readw(base + GXP_I2CSTAT);
-	while(!(value & 0x1000)) {
+	value = readw(priv->base + GXP_I2CSTAT);
+	while (!(value & 0x1000))
+	{
 		udelay(10);
 		timeout += 10;
-		if(POLLTIME_US < timeout) {
+		if (POLLTIME_US < timeout)
+		{
 			return -1;
 		}
-		value = readw(base + GXP_I2CSTAT);
+		value = readw(priv->base + GXP_I2CSTAT);
 	}
 	return 0;
 }
 
-static int gxp_i2c_read(uchar chip, uchar *buffer, int len)
+static int gxp_i2c_read(struct udevice *dev, u8 chip, u8 *buffer, int len)
 {
 	unsigned int value;
 	int i;
-	u32 base;
-
-	base = i2c_base[current_bus];
+	struct gxp_i2c_priv *priv = dev_get_priv(dev);
 
 	/* start + chip + read bit */
 	value = chip << 1;
 	value = value << 8;
 	value |= 0x05;
-	if(bus_state[current_bus] != GXP_I2C_IDLE) {
-		value |= 0x80;	//clear event for restart
+	if (priv->state != GXP_I2C_IDLE)
+	{
+		value |= 0x80; //clear event for restart
 	}
-	writew(value, base + GXP_I2CMCMD);
-	bus_state[current_bus] = GXP_I2C_STARTED;
 
-	if(gxp_wait_event()) {
-		//printf("<%s> bus %d addr phase timeout (I2CSTATE = 0x%04x)\n", __func__, current_bus, value);
+	writew(value, priv->base + GXP_I2CMCMD);
+	priv->state = GXP_I2C_STARTED;
+
+	if (gxp_wait_event(dev))
+	{
+		// printf("<%s> base %x chip %d addr phase timeout (I2CSTATE = 0x%04x)\n", __func__, priv->base, chip, value);
 		return -1;
 	}
 
 	/* check ack */
-	value = readw(base + GXP_I2CSTAT);
-	if(!(value & 0x0008)) {
-		//printf("<%s> bus-%d No ACK for addr phase (STAT=0x%04x)\n", __func__, current_bus, value);
-		//nack
+	value = readw(priv->base + GXP_I2CSTAT);
+	if (!(value & 0x0008))
+	{
+		// printf("<%s> bus-%d No ACK for addr phase (STAT=0x%04x)\n", __func__, chip, value);
+		// nack
 		return -2;
 	}
 
 	/* read data */
-	for (i = 0; i < len; i++) {
+	for (i = 0; i < len; i++)
+	{
 		/* clear event, read, ack*/
 		value = 0x8C;
-		if(len == (i+1)) {
-			value &= ~0x08; //last byte, clear ack enable bit
+		if (len == (i + 1))
+		{
+			value &= ~0x08; //lgxp byte, clear ack enable bit
 		}
-		writeb(value, base + GXP_I2CMCMD);
+		writeb(value, priv->base + GXP_I2CMCMD);
 
-		if(gxp_wait_event()) {
-			//printf("<%s> bus %d data phase timeout (STAT = 0x%04x)\n", __func__, current_bus, value);
+		if (gxp_wait_event(dev))
+		{
+			// printf("<%s> bus %d data phase timeout (STAT = 0x%04x)\n", __func__, chip, value);
 			return -1;
 		}
 
 		/* get the data returned */
-		buffer[i] = readb(base + GXP_I2CSNPDAT);
-		
-		//printf("<%s> bus %d read byte: B%d=0x%02X\n",	__func__, current_bus, i, buffer[i]);
+		buffer[i] = readb(priv->base + GXP_I2CSNPDAT);
 	}
 	return 0;
 }
 
-static int gxp_i2c_write_addr(uchar chip)
+static int gxp_i2c_write_addr(struct udevice *dev, uchar chip)
 {
 	unsigned int value;
-	u32 base;
-
-	base = i2c_base[current_bus];
+	struct gxp_i2c_priv *priv = dev_get_priv(dev);
 
 	/* start + chip(7 bits) + write bit */
 	value = chip << 1;
 	value = value << 8;
 	value |= 0x01;
-	if(bus_state[current_bus] != GXP_I2C_IDLE) {
-		value |= 0x80;	//clear event for restart
+	if (priv->state != GXP_I2C_IDLE)
+	{
+		value |= 0x80; //clear event for restart
 	}
-	writew(value, base + GXP_I2CMCMD);
-	bus_state[current_bus] = GXP_I2C_STARTED;
+	writew(value, priv->base + GXP_I2CMCMD);
+	priv->state = GXP_I2C_STARTED;
 
-	if(gxp_wait_event()) {
-		//printf("<%s> bus %d addr phase timeout (I2CSTATE = 0x%04x)\n", __func__, current_bus, value);
+	if (gxp_wait_event(dev))
+	{
+		// printf("<%s> bus %d base %x addr phase timeout (I2CSTATE = 0x%04x)\n", __func__, chip, priv->base, value);
 		return -1;
 	}
 
 	/* check ack */
-	value = readw(base + GXP_I2CSTAT);
-	if(0x0 == (value & 0x0008)) {
-		//printf("<%s> bus %d No ACK for address phase (STAT=0x%04x)\n", __func__, current_bus, value);
-		//nack
+	value = readw(priv->base + GXP_I2CSTAT);
+	if (0x0 == (value & 0x0008))
+	{
+		// printf("<%s> bus %d No ACK for address phase (STAT=0x%04x)\n", __func__, chip, value);
+		// nack
 		return -2;
 	}
 
 	return 0;
 }
 
-static int gxp_i2c_write_data(uchar *buffer, int len)
+static int gxp_i2c_write(struct udevice *dev, uchar *buffer, int len)
 {
 	unsigned int value;
 	int i;
-	u32 base;
-
-	base = i2c_base[current_bus];
+	struct gxp_i2c_priv *priv = dev_get_priv(dev);
 
 	/* write data */
-	for (i = 0; i < len; i++) {
+	for (i = 0; i < len; i++)
+	{
 		//printf("<%s> bus %d write byte: B%d=0x%02X\n", __func__, current_bus, i, buffer[i]);
 		value = buffer[i];
 		value = value << 8;
 		value |= 0x80;
-		writew(value, base + GXP_I2CMCMD);
+		writew(value, priv->base + GXP_I2CMCMD);
 
-		if(gxp_wait_event()) {
+		if (gxp_wait_event(dev))
+		{
 			//printf("<%s> bus %d data phase timeout (I2CEVTERR = 0x%04x)\n", __func__, current_bus, value);
 			return -1;
 		}
 
 		/* check ack */
-		value = readw(base + GXP_I2CSTAT);
-		if(0x0 == (value & 0x08)) {
+		value = readw(priv->base + GXP_I2CSTAT);
+		if (0x0 == (value & 0x08))
+		{
 			//printf("<%s> bus %d No ACK for data phase (STAT=0x%04x)\n", __func__, current_bus, value);
 			//nack
 			return -2;
@@ -226,135 +220,97 @@ static int gxp_i2c_write_data(uchar *buffer, int len)
 	return 0;
 }
 
-static int gxp_i2c_write(uchar chip, uchar *buffer, int len)
+static int gxp_i2c_read_data(struct udevice *dev, u8 chip_addr, u8 *buffer,
+							 size_t len, bool send_stop)
+{
+	int ret;
+	ret = gxp_i2c_read(dev, chip_addr, buffer, len);
+	if (send_stop)
+		gxp_i2c_stop(dev);
+	return ret;
+}
+
+static int gxp_i2c_write_data(struct udevice *dev, u8 chip_addr, u8 *buffer, size_t len, bool send_stop)
 {
 	int ret;
 
-	ret = gxp_i2c_write_addr(chip);
-	if(ret < 0) {
+	ret = gxp_i2c_write_addr(dev, chip_addr);
+	if (ret < 0)
+	{
+		gxp_i2c_stop(dev);
 		return ret;
 	}
 
-	return gxp_i2c_write_data(buffer, len);
+	ret = gxp_i2c_write(dev, buffer, len);
+	if (send_stop)
+		gxp_i2c_stop(dev);
+	return ret;
 }
 
-//APIs
-
-int i2c_set_bus_num(unsigned int bus)
+static int gxp_i2c_set_speed(struct udevice *dev, unsigned int speed)
 {
-	if (bus >= GXP_I2C_BUS_NUM) {
-		printf("Bad bus: %d\n", bus);
-		return -1;
-	}
 
-	current_bus = bus;
+	return 0;
+}
 
-	if (!bus_initialized[current_bus]) {
-		i2c_bus_init(bus);
+static int gxp_i2c_xfer(struct udevice *dev, struct i2c_msg *msg, int nmsgs)
+{
+	int ret;
+	for (; nmsgs > 0; nmsgs--, msg++)
+	{
+		if (msg->flags & I2C_M_RD)
+		{
+			ret = gxp_i2c_read_data(dev, msg->addr, msg->buf,
+									msg->len, (nmsgs == 1));
+		}
+		else
+		{
+			ret = gxp_i2c_write_data(dev, msg->addr, msg->buf,
+									 msg->len, (nmsgs == 1));
+		}
+		if (ret)
+		{
+			return -EREMOTEIO;
+		}
 	}
 
 	return 0;
 }
 
-unsigned int i2c_get_bus_num(void)
+static int gxp_i2c_probe(struct udevice *dev)
 {
-	return current_bus;
+	debug("Enabling I2C%u\n", dev->seq);
+	gxp_i2c_init_bus(dev);
+	return 0;
 }
 
-void i2c_init(int speed, int slaveaddr)
+static int gxp_i2c_probe_chip(struct udevice *dev, uint chip_addr,
+							  uint chip_flags)
 {
-	current_bus = 0;
-	int i;
 
-	for(i=0;i<GXP_I2C_BUS_NUM;i++) {
-		bus_initialized[current_bus] = 0;
-	}
-
-	i2c_set_bus_num(current_bus);
-}
-
-/*
- * i2c_read: - Read multiple bytes from an i2c device
- *
- * The higher level routines take into account that this function is only
- * called with len < page length of the device (see configuration file)
- *
- * @chip:	address of the chip which is to be read
- * @addr:	i2c data address within the chip
- * @alen:	length of the i2c data address (1..2 bytes)
- * @buffer:	where to write the data
- * @len:	how much byte do we want to read
- * @return:	0 in case of success
- */
-int i2c_read(uchar chip, uint addr, int alen, uchar *buffer, int len)
-{
-	u8 addr_bytes[3];
-	int ret;
-
-	if(alen > 0) {
-		addr_bytes[0] = (u8)((addr >>  0) & 0x000000FF);
-		addr_bytes[1] = (u8)((addr >>  8) & 0x000000FF);
-		addr_bytes[2] = (u8)((addr >> 16) & 0x000000FF);
-		ret = gxp_i2c_write(chip, addr_bytes, alen);
-		if(ret < 0) {
-			gxp_i2c_stop();
-			return ret;
-		}
-	}
-
-	ret = gxp_i2c_read(chip, buffer, len);
-	gxp_i2c_stop();
-	return ret;
-}
-
-/*
- * i2c_probe: - Test if a chip answers for a given i2c address
- *
- * @chip:	address of the chip which is searched for
- * @return:	0 if a chip was found, -1 otherwhise
- */
-int i2c_probe(uchar chip)
-{
 	u8 dummy;
-	return i2c_read(chip, 0, 0, &dummy, 1);
+	debug("Probing chip %d \n", chip_addr);
+
+	return gxp_i2c_read_data(dev, chip_addr, &dummy, 1, true);
 }
 
-/*
- * i2c_write: -  Write multiple bytes to an i2c device
- *
- * The higher level routines take into account that this function is only
- * called with len < page length of the device (see configuration file)
- *
- * @chip:	address of the chip which is to be written
- * @addr:	i2c data address within the chip
- * @alen:	length of the i2c data address (1..2 bytes)
- * @buffer:	where to find the data to be written
- * @len:	how much byte do we want to read
- * @return:	0 in case of success
- */
-int i2c_write(uchar chip, uint addr, int alen, uchar *buffer, int len)
-{
-	u8 addr_bytes[3];
-	int ret;
+static const struct dm_i2c_ops gxp_i2c_ops = {
+	.xfer = gxp_i2c_xfer,
+	.set_bus_speed = gxp_i2c_set_speed,
+	.probe_chip = gxp_i2c_probe_chip,
+};
 
-	ret = gxp_i2c_write_addr(chip);
-	if(ret < 0) {
-		gxp_i2c_stop();
-		return ret;
-	}
+static const struct udevice_id gxp_i2c_ids[] = {
+	{.compatible = "hpe,gxp-i2c"},
+	{.compatible = "hpe,gxp-i2c"},
+	{},
+};
 
-	if(alen > 0) {
-		addr_bytes[0] = (u8)((addr >>  0) & 0x000000FF);
-		addr_bytes[1] = (u8)((addr >>  8) & 0x000000FF);
-		addr_bytes[2] = (u8)((addr >> 16) & 0x000000FF);
-		ret = gxp_i2c_write_data(addr_bytes, alen);
-		if(ret < 0) {
-			gxp_i2c_stop();
-			return ret;
-		}
-	}
-
-	ret = gxp_i2c_write_data(buffer, len);
-	gxp_i2c_stop();
-	return ret;
-}
+U_BOOT_DRIVER(gxp_i2c) = {
+	.name = "gxp_i2c",
+	.id = UCLASS_I2C,
+	.of_match = gxp_i2c_ids,
+	.probe = gxp_i2c_probe,
+	.ops = &gxp_i2c_ops,
+	.priv_auto_alloc_size = sizeof(struct gxp_i2c_priv),
+};
